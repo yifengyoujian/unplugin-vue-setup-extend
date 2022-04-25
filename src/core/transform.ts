ae7c850133642dec0ff3045affd5154be4af167a
张泽凯 <zhangzekai@xrwulian.com>
2022-04-25 15:57:27 +0800

    mag: Project to reconstruct

---
+++ src/core/transform.ts
@@ -0,0 +1,36 @@
+import { parse, compileScript } from '@vue/compiler-sfc'
+import MagicString from 'magic-string'
+
+// fix: Remove the vue-demi  
+const generateScript = (lang: string | true, name: string | true): string =>
+  `<script${
+    lang ? ` lang="${lang}"` : ''
+  }> export default {name: '${name}'}</script>`
+
+export default function transform(code: string, id: string) {
+  const FILENAME_RE = /.*[\\/]*(\S*)/
+  const { descriptor } = parse(code)
+
+  if (descriptor.script && !descriptor.scriptSetup) {
+    return null
+  }
+
+  const magic = new MagicString(code)
+
+  const {
+    attrs: { name, lang },
+  } = compileScript(descriptor, { id })
+
+  name && magic.appendLeft(0, generateScript(lang, name))
+
+  const map = magic.generateMap({ hires: true })
+  const filename = FILENAME_RE.exec(id)![1]
+
+  map.file = filename
+  map.sources = [filename]
+
+  return {
+    map,
+    code: magic.toString(),
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,2 @@
-import { createUnplugin } from 'unplugin'
-import type { Options } from './types'
-import { supportScriptName } from "./lib"
-import { createFilter } from '@rollup/pluginutils'
-
-export default createUnplugin<Options>(options => {
-  const filter = createFilter(
-    [/\.vue$/, /\.vue\?vue/],
-    [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/, /[\\/]\.nuxt[\\/]/],
-  )
-
-
-  return {
-    name: 'unplugin-vue-setup-extend',
-    enforce: 'pre',
-    transformInclude(id) {
-      return filter(id)
-    },
-    async transform(code, id) {
-      if (id.endsWith('.vue') || id.includes('.vue?vue')) {
-        return await supportScriptName.call(this, code, id)
-      }
-      return null
-    },
-  }
-})
+export * from './types'
+export { default } from './core/unplugin'
