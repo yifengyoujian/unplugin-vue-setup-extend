bc39761c26ba3f022da91f651f860123328b2a7b
yifengyoujian <yifengyoujian>
2022-04-25 21:19:26 +0800

    chore: renewal test

--- test/transform.test.ts
+++ test/transform.test.ts
@@ -2,20 +2,22 @@ import { describe, expect, it } from 'vitest'
 import transform from '../src/core/transform'
 
 describe('transform', () => {
-  it('transform script setup', async () => {
+  it('transform script', async () => {
+    const code = `<script setup name="App">// placeholder </script>`
+
+    expect(transform(code, 'App.vue')).toMatchSnapshot()
+  })
+
+  it('Add lang attributes quickly to transformed code', async () => {
     const code = `<script setup lang="ts" name="App">// placeholder </script>`
 
     expect(transform(code, 'App.vue')).toMatchSnapshot()
   })
 
-  it('transform script', async () => {
-    const code = `<script lang="ts" name="App">// placeholder </script>`
-
-    expect(transform(code, 'App.vue')).toMatchSnapshot()
-  })
-
-  it('transform script js', async () => {
-    const code = `<script setup name="App">// placeholder </script>`
+  it('If there is a script that does not convert', async () => {
+    const code =
+      `<script lang="ts">// placeholder </script>
+       <script lang="ts" setup name="App">// placeholder </script>`
 
     expect(transform(code, 'App.vue')).toMatchSnapshot()
   })
