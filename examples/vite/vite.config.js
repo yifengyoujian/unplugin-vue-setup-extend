13e63d9a1a09505cf553dd979de90e2283b4f2fb
yifengyoujian <yifengyoujian>
2022-06-11 20:04:50 +0800

    chore: update the sample

--- examples/vite/vite.config.js
+++ examples/vite/vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Inspect from 'vite-plugin-inspect'
 import VueSetupExtend from "unplugin-vue-setup-extend/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueSetupExtend()],
+  plugins: [Inspect(), vue(), VueSetupExtend()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
